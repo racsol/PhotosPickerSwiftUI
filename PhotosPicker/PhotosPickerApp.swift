b945567db8a5a9d7f1c6acf7e3b713cdd2f8af63
Carlos Silva <csilva@becker.com>
2022-07-29 22:04:01 +0100

    Initial Commit

---
+++ PhotosPicker/PhotosPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerApp.swift
+//  PhotosPicker
+//
+//  Created by Carlos Silva on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
